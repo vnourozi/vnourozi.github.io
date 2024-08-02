11f2c369a8c3b70a268a9aebd2b86b89e212ad4d
vnourozi <138948777+vnourozi@users.noreply.github.com>
2024-08-02 16:16:40 -0600

    Update update_links.py

--- update_links.py
+++ update_links.py
@@ -1,19 +1,19 @@
 import os
 
 def update_links(directory):
-    old_url = "vahidnorozi8.github.io/vahid.github.io"
-    new_url = "vahid.github.io"
+    old_url = "vahidnorozi8.github.io"
+    new_url = "vnourozi.github.io"
     
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith(('.html', '.md', '.txt')):
+            if file.endswith(('.html', '.md', '.txt', '.yml')):
                 filepath = os.path.join(root, file)
-                with open(filepath, 'r') as f:
+                with open(filepath, 'r', encoding='utf-8') as f:
                     content = f.read()
                 
                 if old_url in content:
                     updated_content = content.replace(old_url, new_url)
-                    with open(filepath, 'w') as f:
+                    with open(filepath, 'w', encoding='utf-8') as f:
                         f.write(updated_content)
                     print(f"Updated links in {filepath}")
 
